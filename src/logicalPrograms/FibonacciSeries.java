a356a7d7727160147a8df39f0b6d799c87fbceb6
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-20 15:43:46 +0530

    Welcome Message

---
+++ src/logicalPrograms/FibonacciSeries.java
@@ -0,0 +1,35 @@
+package logicalPrograms;
+
+import java.util.Scanner;
+
+// Fibonacci Series Program
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 /* In Fibonacci Series the series will be sum of the previous 2 Numbers
+		 * Take Scanner Input for first 2 numbers
+		 * Taken For Loop upto 10 Values
+		 * upcoming number will be sum of first two.
+		 * Lets Change secondnd number as first and nextnumber as Second.
+		 * it'll print 10 values in fibonacci series
+		 */
+		System.out.println("Welcome to Fibonacci Series Program");
+//		int firstNumber,secondNumber,nextNumber;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter firstNumber");
+//		firstNumber = sc.nextInt();
+//		System.out.println("Enter secondNumber");
+//		secondNumber = sc.nextInt();
+//		System.out.println("\nThe Fibonacci series upto 10 count ");
+//		System.out.println(firstNumber+"\n"+secondNumber);
+//		for (int i=1;i<=10;i++) {
+//			nextNumber = firstNumber+secondNumber;
+//			firstNumber=secondNumber;
+//			secondNumber=nextNumber;
+//			System.out.println(nextNumber);
+//		}
+
+	}
+
+}
